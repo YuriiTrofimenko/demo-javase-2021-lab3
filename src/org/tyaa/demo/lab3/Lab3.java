7951524d3592db2795b48c3f5fb9b1b1f55815d7
YuriiTrofimenko <tyaa@ukr.net>
2021-05-26 11:40:14 +0300

    natural numbers

--- src/org/tyaa/demo/lab3/Lab3.java
+++ src/org/tyaa/demo/lab3/Lab3.java
@@ -5,6 +5,8 @@
  */
 package org.tyaa.demo.lab3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author yurii
@@ -15,55 +17,22 @@ public class Lab3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int z = 1;
-        int k = 50;
-        int l = 100;
-        do {
-            // 50 + (51) * 0.75 = 88.25
-            // 50 + (51) * 0 = 50
-            // 50 + (51) * 1 = 101
-            int x = (int) (k + (l + 1 - k) * Math.random());
-            System.out.print("x = " + x);
-            if (x % 2 == 0) {
-                System.out.println(" jest liczbą parzystą");
+        
+        System.out.println("Input a natural number:");
+        Scanner sc = new Scanner(System.in);
+        try {
+            int x = sc.nextInt();
+            if (x >= 0) {
+                if (x % 2 == 0) {
+                    System.out.println(" jest liczbą parzystą");
+                } else {
+                    System.out.println(" jest liczbą nieparzystą");
+                }
             } else {
-                System.out.println(" jest liczbą nieparzystą");
+                System.out.println("x must be equals or greather than 0");
             }
-            // z++;
-        } while (++z <= 12);
-        System.out.println("***");
-        z = 1;
-        k = 15;
-        l = 35;
-        do {
-            // 50 + (51) * 0.75 = 88.25
-            // 50 + (51) * 0 = 50
-            // 50 + (51) * 1 = 101
-            int x = (int) (k + (l + 1 - k) * Math.random());
-            System.out.print("x = " + x);
-            if (x % 2 == 0) {
-                System.out.println(" jest liczbą parzystą");
-            } else {
-                System.out.println(" jest liczbą nieparzystą");
-            }
-            // z++;
-        } while (++z <= 12);
-        System.out.println("***");
-        z = 1;
-        k = 320;
-        l = 580;
-        do {
-            // 50 + (51) * 0.75 = 88.25
-            // 50 + (51) * 0 = 50
-            // 50 + (51) * 1 = 101
-            int x = (int) (k + (l + 1 - k) * Math.random());
-            System.out.print("x = " + x);
-            if (x % 2 == 0) {
-                System.out.println(" jest liczbą parzystą");
-            } else {
-                System.out.println(" jest liczbą nieparzystą");
-            }
-            // z++;
-        } while (++z <= 12);
+        } catch (Exception ex) {
+            System.out.println("Wrong number format");
+        }
     }
 }
