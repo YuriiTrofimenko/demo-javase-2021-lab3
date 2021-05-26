5a205329566a4cbe1a71b0a2ec88f2775ed7a61d
YuriiTrofimenko <tyaa@ukr.net>
2021-05-26 11:27:28 +0300

    ranges

--- src/org/tyaa/demo/lab3/Lab3.java
+++ src/org/tyaa/demo/lab3/Lab3.java
@@ -16,9 +16,49 @@ public class Lab3 {
      */
     public static void main(String[] args) {
         int z = 1;
+        int k = 50;
+        int l = 100;
         do {
-            System.out.print("z = " + z);
-            if (z % 2 == 0) {
+            // 50 + (51) * 0.75 = 88.25
+            // 50 + (51) * 0 = 50
+            // 50 + (51) * 1 = 101
+            int x = (int) (k + (l + 1 - k) * Math.random());
+            System.out.print("x = " + x);
+            if (x % 2 == 0) {
+                System.out.println(" jest liczbą parzystą");
+            } else {
+                System.out.println(" jest liczbą nieparzystą");
+            }
+            // z++;
+        } while (++z <= 12);
+        System.out.println("***");
+        z = 1;
+        k = 15;
+        l = 35;
+        do {
+            // 50 + (51) * 0.75 = 88.25
+            // 50 + (51) * 0 = 50
+            // 50 + (51) * 1 = 101
+            int x = (int) (k + (l + 1 - k) * Math.random());
+            System.out.print("x = " + x);
+            if (x % 2 == 0) {
+                System.out.println(" jest liczbą parzystą");
+            } else {
+                System.out.println(" jest liczbą nieparzystą");
+            }
+            // z++;
+        } while (++z <= 12);
+        System.out.println("***");
+        z = 1;
+        k = 320;
+        l = 580;
+        do {
+            // 50 + (51) * 0.75 = 88.25
+            // 50 + (51) * 0 = 50
+            // 50 + (51) * 1 = 101
+            int x = (int) (k + (l + 1 - k) * Math.random());
+            System.out.print("x = " + x);
+            if (x % 2 == 0) {
                 System.out.println(" jest liczbą parzystą");
             } else {
                 System.out.println(" jest liczbą nieparzystą");
